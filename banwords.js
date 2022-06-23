3ad6d3bb22b95940a0c9b9de893948736675475f
gptoys <131796667+gptoys@users.noreply.github.com>
2022-06-23 12:39:50 +0000

    Plugin updates

--- banwords.js
+++ banwords.js
@@ -16,3 +16,27 @@ addWords.addEventListener("click", () => {
 
     // When the file has been read, add its contents to the stoplist.csv file
     reader.onload = () => {
+      // Split the file contents on the comma character
+      const stopwords = reader.result.split(",");
+
+      // Read the stoplist.csv file
+      if (!fs.existsSync("stoplist.csv")) {
+        fs.writeFileSync("stoplist.csv", ""); // creates a new, empty file in case if it doesnt exist
+      }
+      const stoplist = fs.readFileSync("stoplist.csv", "utf8");
+
+      // Loop through the stopwords and add them to the stoplist.csv file if they are not already present
+      stopwords.forEach((word) => {
+        if (!stoplist.includes(word)) {
+          fs.appendFileSync("stoplist.csv", `${word},`);
+        }
+      });
+    };
+  } else {
+    // File is not a CSV file
+    console.log("Please select a CSV file");
+  }
+  document.getElementById(
+    "loadComplete"
+  ).innerHTML = `file ${file.name} has been added to the list.`;
+});
