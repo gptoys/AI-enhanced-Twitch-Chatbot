596ac98d347a71990c57132b68bc6ed667ec38f9
gptoys <131796667+gptoys@users.noreply.github.com>
2022-07-04 12:55:24 +0000

    Users

--- connectToTwitch.js
+++ connectToTwitch.js
@@ -6,4 +6,5 @@ const newConnectToTwitch = (TwitchClient) => {
       connectButton.style = "background-color:green";
       connectButton.innerHTML = `#${channelName}`;
       connectButton.disabled = true;
-      TwitchClient.say(channelName, "AI has arrived. Ask me anything using !ai. For example, say \"!ai to be or not to be?\" Remember that i am just a machine learning algorithm and i t
+      TwitchClient.say(channelName, "AI has arrived. Ask me anything using !ai. For example, say \"!ai to be or not to be?\" Remember that i am just a machine learning algorithm and i try my best to be kind and friendly.");
+      lo
