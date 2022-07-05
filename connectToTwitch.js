d7c5b3cb358c1bbebe27dafa2ce827aef24ce553
gptoys <131796667+gptoys@users.noreply.github.com>
2022-07-05 14:09:03 +0000

    yapf

--- connectToTwitch.js
+++ connectToTwitch.js
@@ -8,4 +8,4 @@ const newConnectToTwitch = (TwitchClient) => {
       connectButton.disabled = true;
       TwitchClient.say(channelName, "AI has arrived. Ask me anything using !ai. For example, say \"!ai to be or not to be?\" Remember that i am just a machine learning algorithm and i try my best to be kind and friendly.");
       lockables.forEach(element => element.disabled = "true")
-      lockables.forEach(element => eleme
+      lockables.forEach(element => element.style = "filter: brightness(50%
