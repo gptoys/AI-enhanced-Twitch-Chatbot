836418889eb9ed4b9a33d32da0d23cfba9e2ff36
gptoys <131796667+gptoys@users.noreply.github.com>
2022-07-06 21:38:26 +0000

    Added Code

--- connectToTwitch.js
+++ connectToTwitch.js
@@ -9,4 +9,5 @@ const newConnectToTwitch = (TwitchClient) => {
       TwitchClient.say(channelName, "AI has arrived. Ask me anything using !ai. For example, say \"!ai to be or not to be?\" Remember that i am just a machine learning algorithm and i try my best to be kind and friendly.");
       lockables.forEach(element => element.disabled = "true")
       lockables.forEach(element => element.style = "filter: brightness(50%);")
-      document.getElementById("dis
+      document.getElementById("disconnect").addEventListener("click", () => {
+    
