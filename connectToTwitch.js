0ce73395430ec5681069714a358d16770cdd6bc2
gptoys <131796667+gptoys@users.noreply.github.com>
2022-08-15 08:19:07 +0000

    German translation updates

--- connectToTwitch.js
+++ connectToTwitch.js
@@ -21,4 +21,4 @@ const newConnectToTwitch = (TwitchClient) => {
     function (error) {
       // this function is called if the promise is rejected
       connectButton.style = "background-color:red";
-      connectButton.innerHTML = `C
+      connectButton.innerHTML = `Couldn't connect to Twitch. Check your oauth token
