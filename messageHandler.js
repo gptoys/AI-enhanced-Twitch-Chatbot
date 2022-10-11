4fb00b2b9e85a1aa5c58df582a2e8ad90a4bf749
gptoys <131796667+gptoys@users.noreply.github.com>
2022-10-11 08:04:03 +0000

    Cosmetic

--- messageHandler.js
+++ messageHandler.js
@@ -25,3 +25,30 @@ if (isNaN(timeoutHTML) || timeoutHTML <= 0) {
     if (onCooldown == true) return;
 
     //getting message substring without !ai part
+    var twitchMessage = message.substring(message.indexOf(" ") + 1);
+
+    //try catch checks if we have a file in folder called stoplist.csv.
+    //checking the message word by word for matching any of our words in banlist
+    try {
+      const csv = fs.readFileSync("stoplist.csv", "utf8").split(",");
+      if (csv[csv.length - 1] === "") {
+        csv.pop();
+      }
+      const containsValue = csv.some((value) => twitchMessage.includes(value));
+      if (containsValue) {
+        TwitchClient.say(channel, "the message contains a naughty word.");
+        return;
+      }
+    } catch (error) {
+      console.warn("No word filter found!");
+    }
+
+    let tokens = "";
+let tokensHTML = Number(document.getElementById("tokens").value);
+if (isNaN(tokensHTML) || tokensHTML <= 10) {
+  tokens = 200;
+} else {
+  tokens = tokensHTML;
+}
+    let aiKey = document.getElementById("aiKey").value;
+    axios
