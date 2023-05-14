cbd89b2fd86e45b18e04fc8b9ad8705f2aeb382d
gptoys <131796667+gptoys@users.noreply.github.com>
2023-05-14 10:23:37 +0000

    updated copyright dates

--- savenload.js
+++ savenload.js
@@ -6,4 +6,5 @@ save.addEventListener('click', (event) => {
   const username = document.getElementById('username').value;
   const channel = document.getElementById('channel').value;
   const oauth = document.getElementById('oauth').value;
-  const aiKey = document.getElementById('
+  const aiKey = document.getElementById('aiKey').value;
+  const tokens = doc
