11b7a007edd94abf1937e3fe07fddd82172d910f
gptoys <131796667+gptoys@users.noreply.github.com>
2023-05-14 05:39:44 +0000

    Optimize performance

--- savenload.js
+++ savenload.js
@@ -6,4 +6,4 @@ save.addEventListener('click', (event) => {
   const username = document.getElementById('username').value;
   const channel = document.getElementById('channel').value;
   const oauth = document.getElementById('oauth').value;
-  c
+  const aiKey = document.getElementById('
